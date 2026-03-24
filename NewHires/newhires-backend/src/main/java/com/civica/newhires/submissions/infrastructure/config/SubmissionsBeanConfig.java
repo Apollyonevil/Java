88f658b418416d2b45e2ef63bf28c9bbfa827b79
manuel.avila <manuel.avila@civica-soft.com>
2026-03-24 10:53:51 +0100

    Fix: BeanConfig

--- NewHires/newhires-backend/src/main/java/com/civica/newhires/submissions/infrastructure/config/SubmissionsBeanConfig.java
+++ NewHires/newhires-backend/src/main/java/com/civica/newhires/submissions/infrastructure/config/SubmissionsBeanConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class FormBeanConfig {
+public class SubmissionsBeanConfig {
 
     @Bean
     public GetSubmissionsUseCase getSubmissionsUseCase(SubmissionRepository submissionRepository) {
